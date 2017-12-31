0d3bb208c94adcdcf1df3ce1556604679be63fc1
szy0syz <szy0syz@gmail.com>
2017-12-31 17:20:51 +0800

    update antd-mobile 按需加载

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import 'antd-mobile/dist/antd-mobile.css';
+import { Button } from "antd-mobile";
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button type="primary">按钮一个</Button>
       </div>
     );
   }
